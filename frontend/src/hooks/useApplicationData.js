ebbc2d7ef757ab75cab3b124c19303741dbc6eaf
Cameron <cameron_uniac@hotmail.com>
2021-08-30 15:12:33 -0400

    Remove original unused logic for displaying all user data

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,38 +1,46 @@
-import {
-    useEffect,
-    useReducer
+import React, {
+    // useEffect,
+    // useReducer,
+    useState
 } from 'react';
-import dataReducer, {
-    SET_USERS
-} from '../reducer/data_reducer';
+// import dataReducer, {
+    // SET_USERS
+// } from '../reducer/data_reducer';
 import axios from 'axios';
 
-const useApplicationData = () => {
-    const [state, dispatch] = useReducer(dataReducer, {
-        users: [],
-        loading: true,
-    });
-    useEffect(() => {
-        axios({
-                method: 'GET',
-                url: '/api/users',
-            })
-            .then(({
-                data
-            }) => {
-                console.log(data);
-                dispatch({
-                    type: SET_USERS,
-                    users: data
-                });
-            })
-            .catch((err) => console.log(err));
-    }, []);
+// const useApplicationData = () => {
+    // const [state, dispatch] = useReducer(dataReducer, {
+        // users: [],
+        // loading: true,
+    // });
+    // useEffect(() => {
+        // axios({
+                // method: 'GET',
+                // url: '/api/users',
+            // })
+            // .then(({
+                // data
+            // }) => {
+                // console.log(data);
+                // dispatch({
+                    // type: SET_USERS,
+                    // users: data
+                // });
+            // })
+            // .catch((err) => console.log(err));
+    // }, []);
 
-    return {
-        state,
-        dispatch,
-    };
-};
+    // return {
+        // state,
+        // dispatch,
+    // };
+// };
+
+const useApplicationData = () => {
+  const [state, setState] = useState({
+  });
+  
+  return {state}
+}
 
 export default useApplicationData;
