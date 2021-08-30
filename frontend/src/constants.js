0ffc927ebf758847a3b9a92902651b051882f8e0
Cameron <cameron_uniac@hotmail.com>
2021-08-30 15:10:57 -0400

    Add constants for login/logout url paths

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,4 @@
 export const api_url = "http://localhost:3001";
-export const api_register = "/register"
+export const api_register = "/register";
+export const api_login = "/login";
+export const api_logout = "/logout";
