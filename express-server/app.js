a7e24323bf5d624514e699d7f5223016d6bbe502
Farid Asadpour <yani5752000@yahoo.com>
2021-08-31 01:09:17 +0000

    navbar

--- express-server/app.js
+++ express-server/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+const bcrypt = require('bcrypt');
 var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session')
+
+
+
+
 var logger = require('morgan');
 const db = require('./db');
 const dbHelpers = require('./helpers/dbHelpers')(db);
@@ -9,6 +15,7 @@ const dbHelpers = require('./helpers/dbHelpers')(db);
 var indexRouter = require('./routes/index');
 var registerRouter = require('./routes/register');
 var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
 
 var app = express();
 
@@ -17,13 +24,20 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["secret"],
+
+  
+  maxAge: 24 * 60 * 60 * 1000 //24 hours
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/register', registerRouter(db));
 app.use('/users', usersRouter);
 app.use('/api/users', usersRouter(dbHelpers));
-
+app.use('/user', userRouter(db));
 
 
 module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
+import { useEffect, useState} from "react";
+import {api_url, api_register} from "./constants"
+import axios from "axios";
 import './App.css';
+import NavBar from './Components/NavBar';
 import useApplicationData from "./hooks/useApplicationData.js";
 // import classNames from "classnames";
 // import Application from "components/Application";
@@ -15,30 +19,64 @@ import {
 
 
 const App = () => {
-  const {
-      state,
-      dispatch
-  } = useApplicationData();
+  const { state, dispatch } = useApplicationData();
+  const [currentUser, setCurrentUser] = useState({})
+//  console.log("state======", state);
+ let user_session = localStorage.getItem("user");
+ if (user_session) {
+  user_session = JSON.parse(user_session);
+  
+ }
+//  user_session = JSON.parse(user_session);
+ console.log("user@@@@@@@", user_session);
+ 
+  useEffect(() => {
+    // axios.get(`${api_url}/user/current`).then((response) => {if (response.status === 200) {
+    //   console.log("data", response.data);
+      // setCurrentUser(response.data);
+      setCurrentUser(user_session);
+    //}})
+  }, [user_session])
+
+  //useEffect(() => {
+    //   axios.get(`${api_url}/user/current`).then((response) => {if (response.status === 200) {
+    //     console.log("data", response.data);
+    //     setCurrentUser(response.data);
+    //   }})
+    // }, [])
+
     const userList = state.users.map((user) => (<li key={user.id} > {user.name} {user.email} </li>
     ));
+    console.log("currentuser", currentUser);
+    const user = state.users.find(user => currentUser === user.id);
+    console.log("user", user);
   return (
-  <Router>
-  <div className="App" >
-   
-    <Route path="/test">
-            <Test />
-    </Route>
-    <Route path="/register">
-            <Register />
-    </Route>
+    <div>
+      <NavBar currentUser={currentUser} user={currentUser}/>
+      <Router>
+      <div className="App" >
+      
+        <Route path="/test">
+                <Test />
+        </Route>
+        <Route path="/register">
+                <Register />
+        </Route>
 
-    <h1> Users </h1>
+        <h1> Users </h1>
 
-    <ul> {userList} </ul>
-  </div >
-  </Router>
+        <ul> {userList} </ul>
+        
+        {/* {currentUser? <div><div>{user.email}</div><a></a></div>  : "hi" } */}
+        
+      </div >
+      </Router>
+    </div>
+    
   );
 };
+
+
 
 
 
