a52d648ad9d545f59af90e6be6ae7d35e3a4aa87
Cameron <cameron_uniac@hotmail.com>
2021-09-02 18:47:48 -0400

    Fix schema and seed errors

--- express-server/db/seeds/07_badges.sql
+++ express-server/db/seeds/07_badges.sql
@@ -5,14 +5,10 @@ VALUES
     ('First Post', '1 Book Read', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
     ('Five Posts', '5 Books Read', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
     ('Ten Posts', '10 Books Read', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('1000 Pages', 'Read 1000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-    ('5000 Pages', 'Read 5000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-    ('10000 Pages', 'Read 10000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-    ('1 Comment', 'Left 1 Comment on a Post', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-    ('5 Comments', 'Left 5 Comment on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-    ('10 Comments', 'Left 10 Comments on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-    ('25 Comments', 'Left 25 Comments on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png')
-
-    
-    
-    
+    ('1000 Pages', 'Read 1000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
+    ('5000 Pages', 'Read 5000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
+    ('10000 Pages', 'Read 10000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
+    ('1 Comment', 'Left 1 Comment on a Post', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
+    ('5 Comments', 'Left 5 Comment on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
+    ('10 Comments', 'Left 10 Comments on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
+    ('25 Comments', 'Left 25 Comments on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png');
