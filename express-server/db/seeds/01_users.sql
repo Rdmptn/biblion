10e9f2fb33ecde507dbc722b914b6a0df974f76e
Cameron <cameron_uniac@hotmail.com>
2021-09-02 18:36:34 -0400

    Update users table and seeds to store data for badge unlocks

--- express-server/db/seeds/01_users.sql
+++ express-server/db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO
-users(name, email, password)
+users(name, email, password, post_count, comment_count)
 VALUES
-    ('Mario Bros', 'mario@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
-    ('Luigi Bros', 'luigi@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
-    ('Princess Peach', 'peach@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
-    ('Princess Daisy', 'daisy@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
-    ('Donkey Kong', 'donkey@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy');
+    ('Mario Bros', 'mario@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy', 1, 0),
+    ('Luigi Bros', 'luigi@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy', 1, 1),
+    ('Princess Peach', 'peach@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy', 1, 1),
+    ('Princess Daisy', 'daisy@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy', 0, 1),
+    ('Donkey Kong', 'donkey@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy', 0, 0);
