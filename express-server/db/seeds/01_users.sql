3881d33dd2d58fd60dd1fe7cdb6be23334c0ad1b
Cameron <cameron_uniac@hotmail.com>
2021-09-02 18:30:44 -0400

    Replace default user passwords with encrypted strings to allow for login

--- express-server/db/seeds/01_users.sql
+++ express-server/db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO
 users(name, email, password)
 VALUES
-    ('Mario Bros', 'mario@nintendo.com', 'test'),
-    ('Luigi Bros', 'luigi@nintendo.com', 'test'),
-    ('Princess Peach', 'peach@nintendo.com', 'test'),
-    ('Princess Daisy', 'daisy@nintendo.com', 'test'),
-    ('Donkey Kong', 'donkey@nintendo.com', 'test');
+    ('Mario Bros', 'mario@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
+    ('Luigi Bros', 'luigi@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
+    ('Princess Peach', 'peach@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
+    ('Princess Daisy', 'daisy@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy'),
+    ('Donkey Kong', 'donkey@nintendo.com', '$2b$10$odajcg2PjKxojhvHk1PKYemeDodRBZqYEAsui.I5QLKUSXVV.DhZy');
