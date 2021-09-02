250cbbc6dd5e4a882d5396d136b4e99f813a06d4
Cameron <cameron_uniac@hotmail.com>
2021-09-02 15:40:48 -0400

    Add axios request to create route for grabbing book data from google api

--- express-server/routes/create.js
+++ express-server/routes/create.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const axios =  require('axios');
 
 module.exports = (db) => {
   
@@ -23,20 +24,42 @@ module.exports = (db) => {
           db.query(`SELECT id FROM categories WHERE topic = $1`, [post.genre])
             .then(data => {
               let categoryId = data.rows[0].id;
-              //Create the new book with associated category id
-              db.query(`INSERT INTO books (category_id, title, author)
-                        VALUES ($1, $2, $3) RETURNING books.id;`, [categoryId, post.title, post.author])
+              //Try to find book on google books api, return pages and cover image if found
+              axios.get(`https://www.googleapis.com/books/v1/volumes?q=intitle:${post.title}+inauthor:${post.author}&country=CA`)
                 .then(data => {
-                  let bookId = data.rows[0].id
-                  db.query(`INSERT INTO posts (user_id, book_id, summary, opinion)
-                            VALUES ($1, $2, $3, $4) RETURNING posts.id;`, [post.user_id, bookId, post.summary, post.opinion])
-                    .then(data => {
-                      res.json(data);
+                  if (data.data.items) {
+                    //Create the new book with associated category id and pages/cover image from api
+                    let cover_url = data.data.items[0].volumeInfo.imageLinks.thumbnail;
+                    let book_pages = data.data.items[0].volumeInfo.pageCount;
+                    db.query(`INSERT INTO books (category_id, title, author, pages, cover_url)
+                            VALUES ($1, $2, $3, $4, $5) RETURNING books.id;`, [categoryId, post.title, post.author, book_pages, cover_url])
+                            .then(data => {
+                      let bookId = data.rows[0].id
+                      //Create the new post
+                      db.query(`INSERT INTO posts (user_id, book_id, summary, opinion)
+                                VALUES ($1, $2, $3, $4) RETURNING posts.id;`, [post.user_id, bookId, post.summary, post.opinion])
+                        .then(data => {
+                          res.json(data);
+                        })
                     })
-                })
-            })  
-        }      
-      })
+                  } else {
+                    //Create the new book with associated category id using default page/image data
+                    db.query(`INSERT INTO books (category_id, title, author)
+                            VALUES ($1, $2, $3) RETURNING books.id;`, [categoryId, post.title, post.author])
+                            .then(data => {
+                      let bookId = data.rows[0].id
+                      //Create the new post 
+                      db.query(`INSERT INTO posts (user_id, book_id, summary, opinion)
+                                VALUES ($1, $2, $3, $4) RETURNING posts.id;`, [post.user_id, bookId, post.summary, post.opinion])
+                        .then(data => {
+                          res.json(data);
+                        })
+                    })
+                  }
+                })  
+              })  
+            }      
+          })
         
       .catch(err => {
         console.log(err);
