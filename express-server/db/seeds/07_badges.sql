f826fffb2ba420d3bae58c79e8d4855b338aa959
Cameron <cameron_uniac@hotmail.com>
2021-09-03 23:56:31 -0400

    Update badge images

--- express-server/db/seeds/07_badges.sql
+++ express-server/db/seeds/07_badges.sql
@@ -5,10 +5,10 @@ VALUES
     ('First Post', '1 Book Read', 'https://i.imgur.com/MJ4Ksw7.png'),
     ('Five Posts', '5 Books Read', 'https://i.imgur.com/lYDLuQH.png'),
     ('Ten Posts', '10 Books Read', 'https://i.imgur.com/kJyqGsW.png'),
-    ('1000 Pages', 'Read 1000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('5000 Pages', 'Read 5000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('10000 Pages', 'Read 10000+ Pages', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('1 Comment', 'Left 1 Comment on a Post', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('5 Comments', 'Left 5 Comment on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('10 Comments', 'Left 10 Comments on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png'),
-    ('25 Comments', 'Left 25 Comments on Posts', 'https://icons.iconarchive.com/icons/chrisl21/minecraft/48/Book-icon.png');
+    ('1000 Pages', 'Read 1000+ Pages', 'https://i.imgur.com/ZKSSPHp.png'),
+    ('5000 Pages', 'Read 5000+ Pages', 'https://i.imgur.com/cpD2l2E.png'),
+    ('10000 Pages', 'Read 10000+ Pages', 'https://i.imgur.com/68ZW3R6.png'),
+    ('1 Comment', 'Left 1 Comment on a Post', 'https://i.imgur.com/gYKroSl.png'),
+    ('5 Comments', 'Left 5 Comment on Posts', 'https://i.imgur.com/41kEBer.png'),
+    ('10 Comments', 'Left 10 Comments on Posts', 'https://i.imgur.com/0okYq6c.png'),
+    ('25 Comments', 'Left 25 Comments on Posts', 'https://i.imgur.com/mTdupnD.png');
